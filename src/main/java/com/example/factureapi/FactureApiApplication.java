f997d2df4530b162234a30332ea6f07e848a0ea0
Taha EDDAKI <taha.eddaki@bpifrance.fr>
2024-09-03 23:12:34 +0200

    Initial commit

---
+++ src/main/java/com/example/factureapi/FactureApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.factureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactureApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FactureApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/factureapi/FactureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.factureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactureApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
